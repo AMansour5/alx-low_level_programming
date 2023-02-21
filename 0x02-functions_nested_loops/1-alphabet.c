bd9ab9b351fc0cc29215d60bc2f5f268ae13a489
AMansour5 <ahmedmonsour5@icloud.com>
2023-02-21 14:48:33 -0800

    h

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,7 +4,7 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+void print_alphabet(void)
 {
 int i;
 for (i = 97; i < 123; i++)
@@ -12,5 +12,4 @@ for (i = 97; i < 123; i++)
 _putchar(i);
 }
 _putchar('\n');
-return(0);
 }
