c3fabc24948c38a305524e2d09f09ddf4f84f0ad
AMansour5 <ahmedmonsour5@icloud.com>
2023-02-27 23:55:29 -0800

    pointers

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,69 +1,24 @@
-#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
  * main - generates keygen.
  * Return: 0 Always.
  */
-
-void randomPasswordGeneration(int N)
+int main(void)
 {
-int i = 0;
+int r = 0, c = 0;
+time_t t;
 
-int randomizer = 0;
-srand((unsigned int)(time(NULL)));
-
-
-char numbers[] = "0123456789";
-
-
-char letter[] = "abcdefghijklmnoqprstuvwyzx";
-
-
-char LETTER[] = "ABCDEFGHIJKLMNOQPRSTUYWVZX";
-
-
-char symbols[] = "!@#$^&*?";
-
-
-char password[N];
-
-
-randomizer = rand() % 4;
-
-
-for (i = 0; i < N; i++) {
-
-if (randomizer == 1) {
-password[i] = numbers[rand() % 10];
-randomizer = rand() % 4;
-printf("%c", password[i]);
-}
-else if (randomizer == 2) {
-password[i] = symbols[rand() % 8];
-randomizer = rand() % 4;
-printf("%c", password[i]);
-}
-else if (randomizer == 3) {
-password[i] = LETTER[rand() % 26];
-randomizer = rand() % 4;
-printf("%c", password[i]);
-}
-else {
-password[i] = letter[rand() % 26];
-randomizer = rand() % 4;
-printf("%c", password[i]);
-}
-}
-}
-
-int main()
+srand((unsigned int) time(&t));
+while (c < 2772)
 {
-
-int N = 10;
-randomPasswordGeneration(N);
-
-return 0;
+r = rand() % 128;
+if ((c + r) > 2772)
+break;
+c = c + r;
+printf("%c", r);
+}
+printf("%c\n", (2772 - c));
+return (0);
 }
