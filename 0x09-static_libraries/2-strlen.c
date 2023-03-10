9350c760829abea31b4903834e419b63637859da
AMansour5 <ahmedmonsour5@icloud.com>
2023-03-10 09:57:08 -0800

    static

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string.
+ * @s: input string.
+ * Return: length of a string.
+ */
+int _strlen(char *s)
+{
+int count; 
+for (count = 0; *(s + count) != '\0'; ++count);
+return (count);
+}
---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _puts - prints a string, followed by a new line.
+ * @str: input string.
+ * Return: no return.
+ */
+void _puts(char *str)
+{
+int count; 
+for (count = 0; *(str + count) != '\0'; ++count){
+_putchar(*(str + count));
+}
+_putchar('\n');
+}
